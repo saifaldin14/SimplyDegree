165597b7a194ddb8cdf6e8bc1173a8357759517e
saifaldin14 <saifaldindeveloper@gmail.com>
2022-02-21 20:12:17 -0500

    Initialized the project with React Flow
    Followed this tutorial to initialize the project:
    
    https://www.youtube.com/watch?v=4Siz4HHJYDQ

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Node from "./components/Node";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Node />
+    </div>
+  );
+}
+
+export default App;
