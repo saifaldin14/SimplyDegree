9a6dd1ca281944633bddeb40df72774323274255
Ben Costas <62489019+bencostas@users.noreply.github.com>
2022-02-22 16:18:19 -0500

    did css layout for signin page (desktop, tablet)

--- src/Screens/SignIn.js
+++ src/Screens/SignIn.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./SignIn.css"
 import StudentDesk from "./Graphics/studentsatdesk.svg"
 import StudentBook from "./Graphics/studentonbook.svg"
 import MainLogo from "./Graphics/mainlogo.svg"
@@ -6,9 +7,18 @@ import MainLogo from "./Graphics/mainlogo.svg"
 function SignIn() {
     return (
         <>
-        <img src={MainLogo} alt="main-logo"/>
-        <img src={StudentDesk} alt="student-desk"/>
-        <img src={StudentBook} alt="student-book"/>
+        <img src={MainLogo} alt="main-logo" className="logo"/>
+        
+        <div className="graphics">
+            <img src={StudentDesk} alt="student-desk"/>
+            <img src={StudentBook} alt="student-book"/>
+        </div>
+
+        <div className="login-button">
+            <h1>
+            Login
+            </h1>
+        </div>
         </>
     );
 
