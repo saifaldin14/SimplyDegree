352fc06b4b92e64089168aa4f31941583d57db57
saifaldin14 <saifaldindeveloper@gmail.com>
2022-02-24 06:47:46 -0500

    Moved the graphics to a dedicated assets folder

--- src/Screens/SignIn.js
+++ src/Screens/SignIn.js
@@ -1,27 +1,24 @@
 import React from "react";
-import "./SignIn.css"
-import StudentDesk from "./Graphics/studentsatdesk.svg"
-import StudentBook from "./Graphics/studentonbook.svg"
-import MainLogo from "./Graphics/mainlogo.svg"
+import "./SignIn.css";
+import StudentDesk from "../assets/signIn/studentsatdesk.svg";
+import StudentBook from "../assets/signIn/studentonbook.svg";
+import MainLogo from "../assets/signIn/mainlogo.svg";
 
 function SignIn() {
-    return (
-        <>
-        <img src={MainLogo} alt="main-logo" className="logo"/>
+  return (
+    <>
+      <img src={MainLogo} alt="main-logo" className="logo" />
 
-        <div className="graphics">
-            <img src={StudentDesk} alt="student-desk" className="student"/>
-            <img src={StudentBook} alt="student-book" className="student"/>
-        </div>
+      <div className="graphics">
+        <img src={StudentDesk} alt="student-desk" className="student" />
+        <img src={StudentBook} alt="student-book" className="student" />
+      </div>
 
-        <div className="login-button">
-            <h1>
-            Log In
-            </h1>
-        </div>
-        </>
-    );
-
+      <div className="login-button">
+        <h1>Log In</h1>
+      </div>
+    </>
+  );
 }
 
 export default SignIn;
