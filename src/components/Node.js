e418cfbe1b0607cd3b19b47990d223644834da03
saifaldin14 <saifaldindeveloper@gmail.com>
2022-02-24 06:44:18 -0500

    Update Node.js

--- src/components/Node.js
+++ src/components/Node.js
@@ -1,77 +1,163 @@
-import React, { useState, Fragment } from "react";
+import React, { useState } from "react";
 
-import ReactFlow, {
-  addEdge,
-  Background,
-  Controls,
-  MiniMap,
-} from "react-flow-renderer";
+import ReactFlow, { removeElements, addEdge } from "react-flow-renderer";
+
+const onLoad = (reactFlowInstance) => reactFlowInstance.fitView();
+
+const onNodeMouseEnter = (event, node) => console.log("mouse enter:", node);
+const onNodeMouseMove = (event, node) => console.log("mouse move:", node);
+const onNodeMouseLeave = (event, node) => console.log("mouse leave:", node);
+const onNodeContextMenu = (event, node) => {
+  event.preventDefault();
+  console.log("context menu:", node);
+};
 
 const initialElements = [
   {
-    id: "1",
+    id: "horizontal-1",
+    sourcePosition: "right",
     type: "input",
-    data: { label: "First Node" },
-    position: { x: 0, y: 0 },
+    className: "dark-node",
+    data: { label: "Input" },
+    position: { x: 0, y: 80 },
+  },
+  {
+    id: "horizontal-2",
+    sourcePosition: "right",
+    targetPosition: "left",
+    data: { label: "A Node" },
+    position: { x: 250, y: 0 },
+  },
+  {
+    id: "horizontal-3",
+    sourcePosition: "right",
+    targetPosition: "left",
+    data: { label: "Node 3" },
+    position: { x: 250, y: 160 },
+  },
+  {
+    id: "horizontal-4",
+    sourcePosition: "right",
+    targetPosition: "left",
+    data: { label: "Node 4" },
+    position: { x: 500, y: 0 },
+  },
+  {
+    id: "horizontal-5",
+    sourcePosition: "top",
+    targetPosition: "bottom",
+    data: { label: "Node 5" },
+    position: { x: 500, y: 100 },
+  },
+  {
+    id: "horizontal-6",
+    sourcePosition: "bottom",
+    targetPosition: "top",
+    data: { label: "Node 6" },
+    position: { x: 500, y: 230 },
+  },
+  {
+    id: "horizontal-7",
+    sourcePosition: "right",
+    targetPosition: "left",
+    data: { label: "Node 7" },
+    position: { x: 750, y: 50 },
+  },
+  {
+    id: "horizontal-8",
+    sourcePosition: "right",
+    targetPosition: "left",
+    data: { label: "Node 8" },
+    position: { x: 750, y: 300 },
+  },
+
+  {
+    id: "horizontal-e1-2",
+    source: "horizontal-1",
+    type: "smoothstep",
+    target: "horizontal-2",
+    animated: true,
+  },
+  {
+    id: "horizontal-e1-3",
+    source: "horizontal-1",
+    type: "smoothstep",
+    target: "horizontal-3",
+    animated: true,
+  },
+  {
+    id: "horizontal-e1-4",
+    source: "horizontal-2",
+    type: "smoothstep",
+    target: "horizontal-4",
+    label: "edge label",
+  },
+  {
+    id: "horizontal-e3-5",
+    source: "horizontal-3",
+    type: "smoothstep",
+    target: "horizontal-5",
+    animated: true,
+  },
+  {
+    id: "horizontal-e3-6",
+    source: "horizontal-3",
+    type: "smoothstep",
+    target: "horizontal-6",
+    animated: true,
+  },
+  {
+    id: "horizontal-e5-7",
+    source: "horizontal-5",
+    type: "smoothstep",
+    target: "horizontal-7",
+    animated: true,
+  },
+  {
+    id: "horizontal-e6-8",
+    source: "horizontal-6",
+    type: "smoothstep",
+    target: "horizontal-8",
+    animated: true,
   },
 ];
-const onLoad = (reactFlowInstance) => {
-  reactFlowInstance.fitView();
-};
 
 const Node = () => {
   const [elements, setElements] = useState(initialElements);
-  const [name, setName] = useState("");
+  const onElementsRemove = (elementsToRemove) =>
+    setElements((els) => removeElements(elementsToRemove, els));
+  const onConnect = (params) => setElements((els) => addEdge(params, els));
+  const changeClassName = () => {
+    setElements((elms) =>
+      elms.map((el) => {
+        if (el.type === "input") {
+          el.className = el.className ? "" : "dark-node";
+        }
 
-  const addNode = () => {
-    setElements((e) =>
-      e.concat({
-        id: (e.length + 1).toString(),
-        data: { label: `${name}` },
-        position: {
-          x: Math.random() * window.innerWidth,
-          y: Math.random() * window.innerHeight,
-        },
+        return { ...el };
       })
     );
   };
 
-  const onConnect = (params) => setElements((e) => addEdge(params, e));
-
   return (
-    <Fragment>
-      <ReactFlow
-        elements={elements}
-        onLoad={onLoad}
-        style={{ width: "100%", height: "90vh" }}
-        onConnect={onConnect}
-        connectionLineStyle={{ stroke: "#ddd", strokeWidth: 2 }}
-        connectionLineType="bezier"
-        snapToGrid={true}
-        snapGrid={[16, 16]}
+    <ReactFlow
+      elements={elements}
+      onElementsRemove={onElementsRemove}
+      onConnect={onConnect}
+      onLoad={onLoad}
+      selectNodesOnDrag={false}
+      onNodeMouseEnter={onNodeMouseEnter}
+      onNodeMouseMove={onNodeMouseMove}
+      onNodeMouseLeave={onNodeMouseLeave}
+      onNodeContextMenu={onNodeContextMenu}
+    >
+      <button
+        onClick={changeClassName}
+        style={{ position: "absolute", right: 10, top: 30, zIndex: 4 }}
       >
-        <Background color="#888" gap={16} />
-        <MiniMap
-          nodeColor={(n) => {
-            if (n.type === "input") return "blue";
-
-            return "#FFCC00";
-          }}
-        />
-        <Controls />
-      </ReactFlow>
-
-      <div>
-        <input
-          type="text"
-          onChange={(e) => setName(e.target.value)}
-          name="title"
-        />
-        <button type="button" onClick={addNode}>
-          Add New Node
-        </button>
-      </div>
-    </Fragment>
+        change class name
+      </button>
+    </ReactFlow>
   );
 };
 
