03898e4d3d356b46704d3695d3d89768947513c3
saifaldin14 <saifaldindeveloper@gmail.com>
2022-03-08 20:48:34 -0500

    Add event to week calendar
    Doesn't save yet

--- src/components/WeeklyStudyPlan.js
+++ src/components/WeeklyStudyPlan.js
@@ -1,10 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import FullCalendar, { formatDate } from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
+import { createEventId } from "../utils/event-utils";
 
 const WeeklyStudyPlan = () => {
+  const [currentEvents, setCurrentEvents] = useState([]);
+  const handleDateSelect = (selectInfo) => {
+    let title = prompt("Please enter a new title for your event");
+    let calendarApi = selectInfo.view.calendar;
+
+    calendarApi.unselect(); // clear date selection
+
+    if (title) {
+      calendarApi.addEvent({
+        id: createEventId(),
+        title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay,
+      });
+    }
+  };
+
+  const handleEventClick = (clickInfo) => {
+    if (
+      window.confirm(
+        `Are you sure you want to delete the event '${clickInfo.event.title}'`
+      )
+    ) {
+      clickInfo.event.remove();
+    }
+  };
+
+  const handleEvents = (events) => {
+    setCurrentEvents(events);
+  };
+
+  function renderEventContent(eventInfo) {
+    return (
+      <>
+        <b>{eventInfo.timeText}</b>
+        <i>{eventInfo.event.title}</i>
+      </>
+    );
+  }
+
   return (
     <div>
       <FullCalendar
@@ -15,6 +57,10 @@ const WeeklyStudyPlan = () => {
         selectMirror={true}
         dayMaxEvents={true}
         weekends={true}
+        select={handleDateSelect}
+        eventContent={renderEventContent} // custom render function
+        eventClick={handleEventClick}
+        eventsSet={handleEvents}
       />
     </div>
   );
