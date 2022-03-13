1a352a44a2b1617714f37994f4830b5eae69cb35
saifaldin14 <saifaldindeveloper@gmail.com>
2022-03-13 19:16:50 -0400

    Update EditNode.js

--- src/components/EditNode.js
+++ src/components/EditNode.js
@@ -1,6 +1,7 @@
-import * as React from "react";
-import { Card } from "@mui/material";
-import Typography from "@mui/material/Typography";
+import React, { useState } from "react";
+import { Card, Typography, TextField, Button } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import SendIcon from "@mui/icons-material/Send";
 
 const style = {
   position: "absolute",
@@ -15,14 +16,61 @@ const style = {
 };
 
 export default function EditNode({ node }) {
+  const [courseName, setCourseName] = useState("");
+  const [courseCode, setCourseCode] = useState("");
+  const [courseDescription, setCourseDescription] = useState("");
+
+  const editCourse = () => {
+    console.log(courseName, " ", courseCode);
+  };
   return (
     <Card sx={style}>
       <Typography id="modal-modal-title" variant="h6" component="h2">
-        Edit Course
+        Edit Course {node.data.label}
       </Typography>
-      <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-        {node.data.label}
-      </Typography>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+          gap: "2rem",
+          marginTop: "2rem",
+          marginBottom: "2rem",
+        }}
+      >
+        <TextField
+          id="outlined-basic"
+          label="Course Name"
+          variant="outlined"
+          onChange={(e) => setCourseName(e.target.value)}
+        />
+        <TextField
+          id="outlined-basic"
+          label="Course Code"
+          variant="outlined"
+          onChange={(e) => setCourseCode(e.target.value)}
+        />
+        <TextField
+          id="outlined-basic"
+          label="Course Description"
+          variant="outlined"
+          onChange={(e) => setCourseDescription(e.target.value)}
+        />
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        <Button variant="contained" endIcon={<SendIcon />} onClick={editCourse}>
+          Edit
+        </Button>
+        {/* <Button variant="outlined" startIcon={<DeleteIcon />}>
+          Cancel
+        </Button> */}
+      </div>
     </Card>
   );
 }
