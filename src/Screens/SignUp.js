505e05dc450f7fd98f5bdf82bc5aa81f49566600
Ben Costas <bencostas@gmail.com>
2022-03-14 14:38:54 -0400

    added verification for email and program inputs

--- src/Screens/SignUp.js
+++ src/Screens/SignUp.js
@@ -3,6 +3,7 @@ import React from "react";
 import { TextField, MenuItem } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import './SignUp.css'
+import { useForm, Controller } from "react-hook-form";
 
 const courses = [
     {
@@ -12,50 +13,80 @@ const courses = [
 
 function SignUp() {
     let navigate = useNavigate();
+    const {handleSubmit, control} = useForm();
+    const onSubmit = data => {
+      console.log(data);
+      navigate("/");
+    }
     return (
         <>
-        <TextField
-          required
-          id="standard-disabled"
-          label="Email"
-          value="email"
-          variant="standard"
-          sx={{width: '25em', marginTop: '10em'}}
+        <Controller
+          name="email"
+          control={control}
+          defaultValue=""
+          render={({field: {onChange, value}, fieldState: {error}}) => (
+            <TextField
+              required
+              id="standard"
+              label="Email"
+              value={value}
+              onChange={onChange}
+              error={!!error}
+              helperText={error ? error.message : null}
+              variant="standard"
+              sx={{width: '25em', marginTop: '10em'}}
+            />
+          )}
+          rules ={{required: 'Email required'}}
+
         />
+        
         <TextField
           required
-          id="standard-disabled"
+          id="standard"
           label="Password"
           type="Password"
-          alue="password"
+          value=""
           variant="standard"
           sx={{width: '25em', marginTop: '1.4em'}}
         />
         <TextField
           required
-          id="standard-disabled"
-          type="Password"
+          id="standard"
+          type="confirmPassword"
           label="Confirm Password"
-          value="confirmPassword"
+          value=""
           variant="standard"
           sx={{width: '25em', marginTop: '1.4em'}}
         />
-        <TextField
-          required
-          id="standard-disabled"
-          select
-          label="Select your Program"
-          value="programName"
-          variant="standard"
-          sx={{width: '25em', marginTop: '1.4em'}}
-        >
-            {courses.map((option) => (
-                <MenuItem key={option.program} value={option.program}>
-                    {option.program}
-                </MenuItem>
-            ))}
-        </TextField>
-        <div className="signup-button" onClick={() => navigate("/")}>
+
+        <Controller
+          name="program"
+          control={control}
+          defaultValue=""
+          render={({field: {onChange, value}, fieldState: {error}}) => (
+            <TextField
+              required
+              id="standard"
+              select
+              label="Select your Program"
+              variant="standard"
+              value={value}
+              onChange={onChange}
+              error={!!error}
+              helperText={error ? error.message : null}
+              sx={{width: '25em', marginTop: '1.4em'}}
+            >
+                {courses.map((option) => (
+                    <MenuItem key={option.program} value={option.program}>
+                        {option.program}
+                    </MenuItem>
+                ))}    
+            </TextField>
+          )}
+          rules ={{required: 'Program required'}}
+        />
+        <div className="signup-button" onClick={handleSubmit(onSubmit)}>
           <h1>Create Account</h1>
         </div>
         </>
