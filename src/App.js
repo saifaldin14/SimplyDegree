c21046c46df83dd9366c4aa290d5ac66bdafb31d
saifaldin14 <saifaldindeveloper@gmail.com>
2022-03-28 07:06:37 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React from "react";
 // import Node from "./components/Node";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import SignIn from "./Screens/SignIn.js";
 import "./App.css";
+import { AuthProvider } from "./utils/context";
 import Home from "./Screens/Home.js";
 import SignUp from "./Screens/SignUp.js";
+import PrivateRoute from "./Screens/PrivateRoute";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<SignIn />} />
-          <Route path="/Home" element={<Home />} />
-          <Route path="/SignUp" element={<SignUp />} />
-        </Routes>
-      </BrowserRouter>
+      <Router>
+        <AuthProvider>
+          <Switch>
+            <PrivateRoute path="/" element={<Home />} />
+            <Route path="/login" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Switch>
+        </AuthProvider>
+      </Router>
     </div>
   );
 }
