71223eff527f3249138d32d832494825fdab1aec
saifaldin14 <saifaldindeveloper@gmail.com>
2022-03-28 06:53:31 -0400

    Init firebase

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React from "react";
 // import Node from "./components/Node";
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignIn from "./Screens/SignIn.js";
 import "./App.css";
 import Home from "./Screens/Home.js";
-import SignUp from "./Screens/SignUp.js"
+import SignUp from "./Screens/SignUp.js";
+import firebase from "firebase/app";
+import "firebase/database";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { firebaseConfig } from "./utils/firebaseConfig";
+
+const firebaseApp = initializeApp(firebaseConfig);
+const fireabseAnalytics = getAnalytics(firebaseApp);
 
 function App() {
   return (
     <div className="App">
-
       <BrowserRouter>
         <Routes>
-            <Route path="/" element={<SignIn/>}/>
-            <Route path="/Home" element={<Home/>}/>
-            <Route path="/SignUp" element={<SignUp/>}/>
+          <Route path="/" element={<SignIn />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/SignUp" element={<SignUp />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/utils/firebaseConfig.js
@@ -0,0 +1,11 @@
+// The only reason not using .env is so that the professor can run and use the application
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCIQA1s07lsQ7rIjnqPrmVz-4Kyqeyn69A",
+  authDomain: "simplydegree.firebaseapp.com",
+  projectId: "simplydegree",
+  storageBucket: "simplydegree.appspot.com",
+  messagingSenderId: "1057909136469",
+  appId: "1:1057909136469:web:ba30efe0bbfea55e7888dc",
+  measurementId: "G-0QEK8NWKJ8",
+};
