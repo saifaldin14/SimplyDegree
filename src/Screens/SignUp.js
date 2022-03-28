52f8e55536f0ab2e7c4590ccb053d667659d2c8b
saifaldin14 <saifaldindeveloper@gmail.com>
2022-03-28 08:48:49 -0400

    Update SignUp.js

--- src/Screens/SignUp.js
+++ src/Screens/SignUp.js
@@ -1,125 +1,93 @@
-
-import React, {useRef} from "react";
+import React, { useRef, useState } from "react";
 import { TextField, MenuItem } from "@mui/material";
-import { useNavigate } from "react-router-dom";
-import './SignUp.css'
+import "./SignUp.css";
 import { useForm, Controller } from "react-hook-form";
+import { Form, Button, Card, Alert } from "react-bootstrap";
+import { useAuth } from "../utils/context";
+import { Link, useNavigate } from "react-router-dom";
 
 const courses = [
-    {
-        program: "Computer Science"
-    }
-]
+  {
+    program: "Computer Science",
+  },
+];
 
 function SignUp() {
-    let navigate = useNavigate();
-    const {handleSubmit, control, watch} = useForm();
+  let navigate = useNavigate();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmRef = useRef();
+  const { signup } = useAuth();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
-    const pword = useRef({});
-    pword.current = watch("password", "");
+  async function handleSubmit(e) {
+    e.preventDefault();
 
-    const onSubmit = data => {
-      console.log(JSON.stringify(data));
-      navigate("/");
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      return setError("Passwords do not match");
     }
-    return (
-        <>
-        <Controller
-          name="email"
-          control={control}
-          defaultValue=""
-          render={({field: {onChange, value}, fieldState: {error}}) => (
-            <TextField
-              required
-              id="standard"
-              label="Email"
-              value={value}
-              onChange={onChange}
-              error={!!error}
-              helperText={error ? error.message : null}
-              variant="standard"
-              sx={{width: '25em', marginTop: '10em'}}
-            />
-          )}
-          rules ={{required: 'Email required'}}
 
-        />
-        <Controller
-          name="password"
-          control={control}
-          defaultValue=""
-          render={({field: {onChange, value}, fieldState: {error}}) => (
+    try {
+      setError("");
+      setLoading(true);
+      await signup(emailRef.current.value, passwordRef.current.value);
+      navigate.push("/");
+    } catch {
+      setError("Failed to create an account");
+    }
+
+    setLoading(false);
+  }
+
+  return (
+    <>
+      {error && <Alert variant="danger">{error}</Alert>}
+      <Form onSubmit={handleSubmit}>
+        <Form.Group id="email">
           <TextField
             required
-            id="standard"
+            id="standard-disabled"
+            label="Email"
+            defaultValue=""
+            variant="standard"
+            sx={{ width: "25em" }}
+            ref={emailRef}
+          />
+        </Form.Group>
+        <Form.Group id="password">
+          <TextField
+            required
+            id="standard-password-input"
             label="Password"
-            type="Password"
-            value={value}
-            onChange={onChange}
-            error={!!error}
-            helperText={error ? error.message : null}
+            type="password"
+            defaultValue=""
             variant="standard"
-            sx={{width: '25em', marginTop: '1.4em'}}
+            sx={{ width: "25em", marginTop: "1.3em" }}
+            ref={passwordRef}
           />
-          )}
-          rules ={{required: 'Password required'}}
-        />
-
-        <Controller
-          name="confirmPassword"
-          control={control}
-          defaultValue=""
-          render={({field: {onChange, value}, fieldState: {error}}) => (
+        </Form.Group>
+        <Form.Group id="password-confirm">
           <TextField
             required
-            id="standard"
+            id="standard-password-input"
+            label="Password Confirmation"
             type="password"
-            label="Confirm Password"
-            value={value}
-            onChange={onChange}
-            error={!!error}
-            helperText={error ? error.message : null}
+            defaultValue=""
             variant="standard"
-            sx={{width: '25em', marginTop: '1.4em'}}
+            sx={{ width: "25em", marginTop: "1.3em" }}
+            ref={passwordConfirmRef}
           />
-          )}
-          rules={{
-            required: "Confirm password required",
-            validate: value => value === pword.current || "Passwords do not match",
-          }}
-        />
-
-        <Controller
-          name="program"
-          control={control}
-          defaultValue=""
-          render={({field: {onChange, value}, fieldState: {error}}) => (
-            <TextField
-              required
-              id="standard"
-              select
-              label="Select your Program"
-              variant="standard"
-              value={value}
-              onChange={onChange}
-              error={!!error}
-              helperText={error ? error.message : null}
-              sx={{width: '25em', marginTop: '1.4em'}}
-            >
-                {courses.map((option) => (
-                    <MenuItem key={option.program} value={option.program}>
-                        {option.program}
-                    </MenuItem>
-                ))}    
-            </TextField>
-          )}
-          rules ={{required: 'Program required'}}
-        />
-        <div className="signup-button" onClick={handleSubmit(onSubmit)}>
-          <h1>Create Account</h1>
-        </div>
-        </>
-    );
+        </Form.Group>
+        <Button disabled={loading} className="signup-button" type="submit">
+          Sign Up
+        </Button>
+      </Form>
+      <div className="w-100 text-center mt-2">
+        Already have an account? <Link to="/login">Log In</Link>
+      </div>
+    </>
+  );
 }
 
 export default SignUp;
